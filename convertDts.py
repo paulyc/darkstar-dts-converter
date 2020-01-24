d698c92d9fb32be91d03b68b1a28a9de5441172a
Matthew Rindel <matthew@sobantu.co.za>
2020-01-24 16:50:17 +0200

    Added first broken version of code

--- convertDts.py
+++ convertDts.py
@@ -1,13 +1,11 @@
 
 import sys
 import json
-import glob
 
 import readDts
 import mapObjects
 import writeObj
-
-from collections import namedtuple
+import volinfo
 
 with open('structures.json') as f:
     structures = json.load(f)
@@ -15,28 +13,34 @@ with open('structures.json') as f:
 importFilenames = sys.argv[1:]
 
 for importFilename in importFilenames:
-    exportFilename = importFilename.replace(".dts", ".obj").replace(".DTS", ".obj")
-    exportFilenameWithoutExtension = exportFilename.split(".obj")[0]
-    print("reading " + importFilename)
-    try:
-        with open(importFilename, "rb") as input_fd:
-            # first get the parsed shape datastructures
-            rawData = input_fd.read()
-            shape = readDts.readDtsData(structures, rawData)
-        # then map them for conversation later
-        mappedDetails = mapObjects.mapObjects(shape, False)
+    if importFilename.endswith("vol") or importFilename.endswith("VOL"):
+        with open(importFilename, "rb") as volFile:
+            rawVolFile = volFile.read()
 
-        for detail in mappedDetails:
-            try:
-                exportFilename.split("0")
-                newFilename = exportFilenameWithoutExtension + "_" + detail.rootNode.name.decode("utf-8") + ".obj"
-                print("writing " + newFilename)
-                # save a new file
-                with open(newFilename,"w") as shapeFile:
-                    #TODO move the normal table out of the obj writer into the object mapper
-                    writeObj.writeObj(detail.rootNode, structures["normalTable"], shapeFile)
-                print("completed " + newFilename)
-            except Exception as e:
-                print(e)
-    except Exception as e:
-        print(e)
+        file_info = archive_module.get_file_metadata(raw_data)
+    else:
+        exportFilename = importFilename.replace(".dts", ".obj").replace(".DTS", ".obj")
+        exportFilenameWithoutExtension = exportFilename.split(".obj")[0]
+        print("reading " + importFilename)
+        try:
+            with open(importFilename, "rb") as input_fd:
+                # first get the parsed shape datastructures
+                rawData = input_fd.read()
+                shape = readDts.readDtsData(structures, rawData)
+            # then map them for conversation later
+            mappedDetails = mapObjects.mapObjects(shape, False)
+
+            for detail in mappedDetails:
+                try:
+                    exportFilename.split("0")
+                    newFilename = exportFilenameWithoutExtension + "_" + detail.rootNode.name.decode("utf-8") + ".obj"
+                    print("writing " + newFilename)
+                    # save a new file
+                    with open(newFilename,"w") as shapeFile:
+                        #TODO move the normal table out of the obj writer into the object mapper
+                        writeObj.writeObj(detail.rootNode, structures["normalTable"], shapeFile)
+                    print("completed " + newFilename)
+                except Exception as e:
+                    print(e)
+        except Exception as e:
+            print(e)
---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='darkstar-dts-converter',
+    version='0.0.1',
+    packages=[''],
+    url='https://github.com/matthew-rindel/darkstar-dts-converter',
+    license='MIT',
+    author='Matthew',
+    author_email='matthew@sobantu.co.za',
+    description=''
+)
